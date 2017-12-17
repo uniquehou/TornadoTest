cfa73aab97ca027ed589b2be60d9faa19dcd58f9
houyajie <919863463@qq.com>
2017-12-17 21:13:34 +0800

    添加static静态路径

--- TornadoTest/settings.py
+++ TornadoTest/settings.py
@@ -127,4 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/TornadoTest/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
